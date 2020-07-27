28fb9d49daae67953acf35477c0df45b3189ee53
JefteCosta <jefteamorim@gmail.com>
2020-07-26 21:01:17 -0300

    primeiro commit do sitema

---
+++ src/api/layers/sender.layer.ts
@@ -0,0 +1,545 @@
+/*
+NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mMMMMMMMMMNNNmmNNNMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNMMNMMMMNNNNNmmmddhdddNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mddNMMNy:/odNmmddmmNNmdhhddmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmmdNMNd:--+dNmmddhhddmmhsyhhmdmmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNmdNmy:.-oyNmmmhmdhho+sososyhhhddNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmmNdh+-`.:oyNNdmmdmmdo-://oysssyhhhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+Nmmmoyyyo+osdNmdmmddNNhs+/::/+osyssydyhdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NNmhsymMMNmmmmdmdNNddNmsso+++////ossssyyhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mhhhmNNMNNNhssshhmmddmmssyooooso/::+oysshhhhmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmdhdddNNdyoosyhdmddmmmsoooooyysyys/::/oyyhhhyMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mdddhddmhsooshdmdmdhhyyyysso/ooo+syhhs/-/+shyhMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+dyyhdmd+ososhdmdmyyhhhhhhhyo++o/+///+ohhso++sdMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+dhdmNNdsossyhmdmsydhssssyhhs/++o/o+//:++yhhy+/hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mdmNNNNmhysshddyshdyyy/oss+s::/:://++///++++/::hmNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NNMNNNmmNNdymNNhshdshdyhdysh+sy+-:++osssosss++yNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNNNmdNNmNmmmNmyyddyyhdhydyohys/-oo+osssysyyohNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNNNhdNmmNNmNMMNhyyhhhdhyyhmmyh+-/s+sysssyyhyydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mNMMMhdNdmMNMMMMMNNmdhdddmhdmmNho/-osoyyo++oyddhhNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NMMMNmhNdNMNMNMMNmNNNmmmdyoohmhoyo::hsooo++oooydhymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMNNNhmNNMmmNMNNmmmmdmmdyhhoyddddoo++yoyysooossyhsmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMNNNmmNNNmdNdNmmddhhhdNNhsmNssdooo/dso++osyyysoymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMNNNNmNNNNNmddmmNhshNmmmNmNMdhNsh/ohho++/:++MMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MNNNMMNNNNmmmhhhhdyosdNmdmMMhoNmhdmys+ooo++/+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNNNMMNNNNmddmdoodmMMNmmNNhssdmNMMMNdNd/osomMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNdhMNmNNMNmdNddohmMMNNNmdmdddNMMMMMMMMmMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNhmMmmmmNNmdNyoNMNmNmdhyyyhdhoyNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmdmMmmddddNmmdys+hmMMMmmhysssyy++dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmdNMMdmdddmmNNyshmNNNNNNNdhhs+yy//dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNMMMdmdddmmMNysdmNNMMMNhhNdhs+y+/:mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNMMNhmmddNNNMdyydmMMMNdyshNhyoss+:/MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNMMddmmmmNMNMNdsymNNmdhhdNMNdhsss+:yMMMMMMMMMMMMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMdhmmmmmNMNNMmshNMMMmmMMMMMmNdyo+//NMMMMMMMMMMMMMMMhNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMmhmmmmmmNMMNNMyshdhhhyhNMMMMMMdhso+sMMMMMMMMMMMMMMMhmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMmdmmmmmmmNMMMmNm+ys++oyyNMMMMMMNmmyyoyNMMMMMMMMMMMMMddMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmmmmmmmmmmmNMNNmNNyyo+/oohNMMMMMMMMdhhsshmMMMMMMMMMMMyNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNNNNNNmmmmNMMNmmddNmmdhhdmMMMMMMMMMNddhssshmmNNNmmdhdMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NNNNNNNNNNNNNNNNmNNNNMMMMMNomMMMMMMMMMNNmdhhyyyyyyyhdmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+Nd+oNMMMMMMMmodo++++++++++m..yNMMMMMNo+mNMMmhssshdNMMNhNMMMMMMMMMMMddMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MN+ /NMMMMMm: d` -ssssss+`d. `+mMMMMN. dNm+:+syso//hNN--yNMMMMMMMd+`yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMN+ /NMMMm: oM` +NMMMMMNdN. /`.yNMMN. dh.omMMMMMNy.oM- `:hNMMMm+.  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMN/ /NMm: oNy` :sssmMMMMN. dh-`/mMN. d-/NMMMMMMMMy`m- y/`/dmo..o: yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMN/ /m: +NNy. /yyyNMMMMN. dNNo`.yN- d.oNMMMMMMMMd d- mNh-`.`+mN/ yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMN/ . +NMMN- oNMMMMMNdN. dMMMd:`/. ds.dNMMMMMMm::M- dMMNy/dMMN/ yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMN/ +NMMMN- /yyyyyys d. dMMMMNo`  dNy-+ymmmho-+NN- dMMMMMMMMN/ yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMNyNMMMMN+::::::::::m+/mMMMMMMd: dMMNho///+ymMMN+/mMMMMMMMMNs/hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMNsmMMMMMMMMMMMMMMNNNNMMNNNMMNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMMNMMNMNMMMNMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMNMNMMMNMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNMMNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+all copyright reservation for S2 Click, Inc
+*/
+import { Page } from 'puppeteer';
+import * as sharp from 'sharp';
+import { base64MimeType, fileToBase64, dowloadFileImgHttp, stickerSelect} from '../helpers';
+import { Chat, Message, Contact } from '../model';
+import { ChatState } from '../model/enum';
+import { ListenerLayer } from './listener.layer';
+import axios, { AxiosRequestConfig } from 'axios';
+import { Url } from 'url';
+
+
+declare module WAPI {
+  const sendSeen: (to: string) => void;
+  const getChat: (contactId: string) => Chat;
+  const startTyping: (to: string) => void;
+  const stopTyping: (to: string) => void;
+  const sendMessage: (to: string, content: string) => string;
+  const sendImage: (
+    imgBase64: string,
+    to: string,
+    filename: string,
+    caption: string
+  ) => any;
+  const sendMessageWithThumb: (
+    thumb: string,
+    url: string,
+    title: string,
+    description: string,
+    chatId: string
+  ) => void;
+  const reply: (
+    to: string,
+    content: string,
+    quotedMsg: string | Message,
+    mentioned: string[]
+  ) => void;
+  const sendPtt: (
+    base64: string,
+    to: string,
+    filename: string,
+    caption: string
+  ) => any;
+  const sendFile: (
+    base64: string,
+    to: string,
+    filename: string,
+    caption: string
+  ) => any;
+  const sendVideoAsGif: (
+    base64: string,
+    to: string,
+    filename: string,
+    caption: string
+  ) => void;
+  const sendContact: (to: string, contact: string | string[]) => any;
+  const forwardMessages: (
+    to: string,
+    messages: string | string[],
+    skipMyMessages: boolean
+  ) => any;
+  const sendImageAsSticker: ( 
+    webpBase64: string, 
+    to: string, metadata?: any 
+    ) => boolean;
+  const sendImageAsStickerGif: ( 
+    webpBase64: string, 
+    to: string, 
+    metadata?: any 
+    ) => boolean;
+  const sendLocation: ( to: string, latitude: any, longitude: any, caption: string ) => Promise<string>;
+  const sendMessageMentioned: (...args: any) => any;
+  const sendMessageToID: (id: string, message: string) => any;
+  const setChatState: (chatState: string, chatId: string) => void;
+  const sendLinkPreview: (chatId: string, url: string, title:string) => Promise<string>;
+}
+
+export class SenderLayer extends ListenerLayer {
+  constructor(public page: Page) {
+    super(page);
+  }
+
+
+/**
+ * Automatically sends a link with the auto generated link preview. You can also add a custom message to be added.
+ * @param chatId 
+ * @param url string A link, for example for youtube. e.g https://www.youtube.com/watch?v=Zi_XLOBDo_Y&list=RDEMe12_MlgO8mGFdeeftZ2nOQ&start_radio=1
+ * @param title custom text as the message body, this includes the link or will be attached after the link
+ */
+  public async sendLinkPreview(chatId: string, url: string, title: string){
+    return await this.page.evaluate(
+      ({ chatId, url, title}) => {  return WAPI.sendLinkPreview(chatId, url, title); }, {chatId, url, title}
+    );
+  }
+
+  /**
+   * Sends a text message to given chat
+   * @param to chat id: xxxxx@us.c
+   * @param content text message
+   */
+  public async sendText(to: string, content: string): Promise<string> {
+    return this.page.evaluate(
+      ({ to, content }) => {
+        WAPI.sendSeen(to);
+        if (!WAPI.getChat(to)) {
+          return WAPI.sendMessageToID(to, content);
+        } else {
+          return WAPI.sendMessage(to, content);
+        }
+      },
+      { to, content }
+    );
+  }
+
+  /**
+   * Experimental!
+   * Sends a text message to given chat even if its a non-existent chat
+   * @param to chat id: xxxxx@us.c
+   * @param content text message
+   */
+  public async sendMessageToId(to: string, content: string): Promise<string> {
+    return this.page.evaluate(
+      ({ to, content }) => {
+        WAPI.sendSeen(to);
+        return WAPI.sendMessageToID(to, content);
+      },
+      { to, content }
+    );
+  }
+
+  /**
+   * Sends image message
+   * @param to Chat id
+   * @param imgBase64
+   * @param filename
+   * @param caption
+   */
+  public async sendImage(
+    to: string,
+    path: string,
+    filename: string,
+    caption?: string
+  ) {
+
+
+    let data = await dowloadFileImgHttp(path, ['image/png','image/jpg', 'image/webp', 'image/gif']);
+    if(!data){
+      data = await fileToBase64(path);
+    }
+
+    return this.page.evaluate(
+      ({ to, data, filename, caption }) => {
+        WAPI.sendImage(data, to, filename, caption);
+      },
+      { to, data, filename, caption }
+    );
+  }
+
+  /**
+   * Sends image message
+   * @param to Chat id
+   * @param imgBase64
+   * @param filename
+   * @param caption
+   */
+  public async sendImageFromBase64(
+    to: string,
+    base64: string,
+    filename: string,
+    caption?: string
+  ) {
+    return this.page.evaluate(
+      ({ to, base64, filename, caption }) => {
+        WAPI.sendImage(base64, to, filename, caption);
+      },
+      { to, base64, filename, caption }
+    );
+  }
+
+  /**
+   * Sends message with thumbnail
+   * @param thumb
+   * @param url
+   * @param title
+   * @param description
+   * @param chatId
+   */
+  public async sendMessageWithThumb(
+    thumb: string,
+    url: string,
+    title: string,
+    description: string,
+    chatId: string
+  ) {
+    return this.page.evaluate(
+      ({ thumb, url, title, description, chatId }) => {
+        WAPI.sendMessageWithThumb(thumb, url, title, description, chatId);
+      },
+      {
+        thumb,
+        url,
+        title,
+        description,
+        chatId,
+      }
+    );
+  }
+
+  /**
+   * Replies to given mesage id of given chat id
+   * @param to Chat id
+   * @param content Message body
+   * @param quotedMsg Message id to reply to.
+   * @param mentioned User id to mentioned (just phone number).
+   */
+  public async reply(
+    to: string,
+    content: string,
+    quotedMsg: string,
+    mentioned: string[]
+  ) {
+    return await this.page.evaluate(
+      ({ to, content, quotedMsg, mentioned }) => {
+        return WAPI.reply(to, content, quotedMsg, mentioned);
+      },
+      { to, content, quotedMsg, mentioned }
+    );
+  }
+
+  /**
+   * Sends file
+   * base64 parameter should have mime type already defined
+   * @param to Chat id
+   * @param base64 base64 data
+   * @param filename
+   * @param caption
+   */
+  public async sendPttFromBase64(
+    to: string,
+    base64: string,
+    filename: string,
+    caption?: string
+  ) {
+    return this.page.evaluate(
+      ({ to, base64, filename, caption }) => {
+        WAPI.sendPtt(base64, to, filename, caption);
+      },
+      { to, base64, filename, caption }
+    );
+  }
+
+  /**
+   * Sends file
+   * base64 parameter should have mime type already defined
+   * @param to Chat id
+   * @param base64 base64 data
+   * @param filename
+   * @param caption
+   */
+  public async sendFileFromBase64(
+    to: string,
+    base64: string,
+    filename: string,
+    caption?: string
+  ) {
+    return this.page.evaluate(
+      ({ to, base64, filename, caption }) => {
+        WAPI.sendFile(base64, to, filename, caption);
+      },
+      { to, base64, filename, caption }
+    );
+  }
+
+  /**
+   * Sends file from path
+   * @param to Chat id
+   * @param path File path
+   * @param filename
+   * @param caption
+   */
+  public async sendFile(
+    to: string,
+    path: string,
+    filename: string,
+    caption?: string
+  ) {
+    const base64 = await fileToBase64(path);
+    return this.sendFileFromBase64(to, base64, filename, caption);
+  }
+
+  /**
+   * Sends a video to given chat as a gif, with caption or not, using base64
+   * @param to chat id xxxxx@us.c
+   * @param base64 base64 data:video/xxx;base64,xxx
+   * @param filename string xxxxx
+   * @param caption string xxxxx
+   */
+  public async sendVideoAsGif(
+    to: string,
+    path: string,
+    filename: string,
+    caption: string
+  ) {
+    const base64 = await fileToBase64(path);
+    return this.sendVideoAsGifFromBase64(to, base64, filename, caption);
+  }
+
+  /**
+   * Sends a video to given chat as a gif, with caption or not, using base64
+   * @param to chat id xxxxx@us.c
+   * @param base64 base64 data:video/xxx;base64,xxx
+   * @param filename string xxxxx
+   * @param caption string xxxxx
+   */
+  public async sendVideoAsGifFromBase64(
+    to: string,
+    base64: string,
+    filename: string,
+    caption: string
+  ) {
+    return await this.page.evaluate(
+      ({ to, base64, filename, caption }) => {
+        WAPI.sendVideoAsGif(base64, to, filename, caption);
+      },
+      { to, base64, filename, caption }
+    );
+  }
+
+  /**
+   * Sends contact card to iven chat id
+   * @param to Chat id
+   * @param contactsId Example: 0000@c.us | [000@c.us, 1111@c.us]
+   */
+  public async sendContact(to: string, contactsId: string | string[]) {
+    return this.page.evaluate(
+      ({ to, contactsId }) => WAPI.sendContact(to, contactsId),
+      { to, contactsId }
+    );
+  }
+
+  /**
+   * Forwards array of messages (could be ids or message objects)
+   * @param to Chat id
+   * @param messages Array of messages ids to be forwarded
+   * @param skipMyMessages
+   */
+  public async forwardMessages(
+    to: string,
+    messages: string | string[],
+    skipMyMessages: boolean
+  ) {
+    return this.page.evaluate(
+      ({ to, messages, skipMyMessages }) =>
+        WAPI.forwardMessages(to, messages, skipMyMessages),
+      { to, messages, skipMyMessages }
+    );
+  }
+  
+/**
+  * Generates sticker from the provided animated gif image and sends it (Send image as animated sticker)
+  *  @param path image path imageBase64 A valid gif image is required. You can also send via http/https (http://www.website.com/img.gif)
+  *  @param to chatId '000000000000@c.us'
+  */
+  public async sendImageAsStickerGif(to: string, path: string) {
+    let b64 = await dowloadFileImgHttp(path, ['image/gif']);
+    if(!b64){
+      b64 = await fileToBase64(path);
+    }
+  const buff = Buffer.from(
+    b64.replace(/^data:image\/(gif);base64,/,''),
+    'base64'
+  );
+  const mimeInfo = base64MimeType(b64);
+  if(!mimeInfo || mimeInfo.includes('image')){
+    let obj = await stickerSelect(buff, 1);
+    if(typeof obj == 'object'){
+      let _webb64 = obj["webpBase64"];
+         let _met = obj["metadata"]; 
+      return await this.page.evaluate(
+        ({ _webb64, to, _met }) =>
+          WAPI.sendImageAsSticker(_webb64, to, _met),
+        { _webb64, to, _met }
+      );
+    }
+      }else{
+      console.log('Not an image, allowed format gif');
+      return false;
+   }
+  }
+
+  /**
+   * Generates sticker from given image and sends it (Send Image As Sticker)
+   * @param path image path imageBase64 A valid png, jpg and webp image is required. You can also send via http/https (http://www.website.com/img.gif)
+   * @param to chatId '000000000000@c.us'
+   */
+  public async sendImageAsSticker(to: string, path: string) {
+
+    let b64 = await dowloadFileImgHttp(path, ['image/png','image/jpg', 'image/webp']);
+      if(!b64){
+        b64 = await fileToBase64(path);
+      }
+    const buff = Buffer.from(
+      b64.replace(/^data:image\/(png|jpeg|webp);base64,/,''),
+      'base64'
+    );
+    const mimeInfo = base64MimeType(b64);
+
+    if (!mimeInfo || mimeInfo.includes('image')) {
+      let obj = await stickerSelect(buff, 0);
+      if(typeof obj == 'object'){
+        let _webb64 = obj["webpBase64"];
+           let _met = obj["metadata"]; 
+        return await this.page.evaluate(
+          ({ _webb64, to, _met }) =>
+            WAPI.sendImageAsSticker(_webb64, to, _met),
+          { _webb64, to, _met }
+        );
+      }
+    } else {
+      console.log('Not an image, allowed formats png, jpeg and webp');
+      return false;
+    }
+  }
+
+  /**
+   * TODO: Fix message not being delivered
+   * Sends location to given chat id
+   * @param to Chat id
+   * @param latitude Latitude
+   * @param longitude Longitude
+   * @param caption Text caption
+   */
+  public async sendLocation( to: string, latitude: any, longitude: any, title?: string, subtitle?: string ) {
+    // Create caption
+    let caption = title || '';
+    if (subtitle) {
+      caption = `${title}\n${subtitle}`;
+    }
+    return await this.page.evaluate(({ to, latitude, longitude, caption }) => { WAPI.sendLocation(to, latitude, longitude, caption); },  {to, latitude, longitude, caption} );
+  }
+
+  /**
+   * Sets a chat status to seen. Marks all messages as ack: 3
+   * @param chatId chat id: xxxxx@us.c
+   */
+  public async sendSeen(chatId: string) {
+    return this.page.evaluate((chatId) => WAPI.sendSeen(chatId), chatId);
+  }
+
+  /**
+   * Starts typing ('Typing...' state)
+   * @param chatId
+   */
+  public async startTyping(to: string) {
+    return this.page.evaluate(({ to }) => WAPI.startTyping(to), { to });
+  }
+
+  /**
+   * Stops typing
+   * @param to Chat id
+   */
+  public async stopTyping(to: string) {
+    return this.page.evaluate(({ to }) => WAPI.stopTyping(to), { to });
+  }
+
+  /**
+   * Sends text with tags
+   *
+   */
+  public async sendMentioned(to: string, message: string, mentioned: string[]) {
+    return await this.page.evaluate(
+      ({ to, message, mentioned }) => {
+        WAPI.sendMessageMentioned(to, message, mentioned);
+      },
+      { to, message, mentioned }
+    );
+  }
+
+  /**
+   * Sets the chat state
+   * @param chatState
+   * @param chatId
+   */
+  public async setChatState(chatId: string, chatState: ChatState) {
+    return await this.page.evaluate(
+      ({ chatState, chatId }) => {
+        WAPI.setChatState(chatState, chatId);
+      },
+      { chatState: chatState, chatId }
+    );
+  }
+}
